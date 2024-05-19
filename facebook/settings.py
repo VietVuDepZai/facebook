dd214cc4334f4ed4624998a36a9f4653c97e3e21
vietmrvu <viet.mrvu@gmail.com>
2024-05-20 06:29:12 +0700

    first commit

--- facebook/settings.py
+++ facebook/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = 'facebook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cqvfoagb', 
+        'USER': 'cqvfoagb', 
+        'PASSWORD': 'TbouI9lf1scPPY0oQNg012mMqpAK_GqB',
+        'HOST': 'floppy.db.elephantsql.com', 
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
